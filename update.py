6d03b4aba825262a1892b8e4a097f82abf8a55d0
sitius2 <35724051+sitius2@users.noreply.github.com>
2018-02-15 21:22:20 +0100

    Adding py file

---
+++ update.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import re
+import os
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description="Create index.html from directory contents")
+
+class HtmlPageCreator:
+    def __init__(self, ):
